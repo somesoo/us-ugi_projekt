365b083823fceef15772b32ac7bd9ef15bbe1ece
somesoo <jakasperczak@gmail.com>
2022-12-21 20:59:02 +0100

    multiple per item

--- Symulacja/baza.py
+++ Symulacja/baza.py
@@ -13,13 +13,12 @@ for item in range(len(json_obj["Boards"])):
     where = json_obj["Boards"][item]["Placement"]
     cursor.execute("insert into Boards(ID,Name,Placement) value(%s, %s, %s)",(id,name,where))
 
-cursor.close()
-
 cursor=connection.cursor()
 for item in range(len(json_obj["Worker"])):
     name = json_obj["Worker"][item]["Name"]
-    inventory = json_obj["Worker"][item]["inv"]
-    cursor.execute("insert into Workers (Name,Inventory) value (%s, %s)",(name,inventory))
-cursor.close()
+    for i in range(len(json_obj["Worker"][item]["inv"])):
+        inventory = json_obj["Worker"][i]["inv"]
+        cursor.execute("insert into Workers (Name,Inventory) value (%s, %s)",(name,inventory))
+        
 connection.commit()
 connection.close()
